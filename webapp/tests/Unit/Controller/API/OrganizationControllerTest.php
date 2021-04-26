b846e8cefc0667b1e793ec61411c1709dd8949f1
Nicky Gerritsen <nickygerritsen@me.com>
2021-04-26 11:42:31 +0200

    Add tests for logo/photo/banner.
    Also add generic contest API tests.

--- webapp/tests/Unit/Controller/API/OrganizationControllerTest.php
+++ webapp/tests/Unit/Controller/API/OrganizationControllerTest.php
@@ -13,9 +13,43 @@ class OrganizationControllerTest extends BaseTest
             'id'          => '1',
             'name'        => 'UU',
             'formal_name' => 'Utrecht University',
-            'country'     => 'NLD'
+            'country'     => 'NLD',
+            'logo'           => [
+                [
+                    'href'   => 'contests/2/organizations/1/logo.png',
+                    'mime'   => 'image/png',
+                    'width'  => 181,
+                    'height' => 101
+                ]
+            ]
         ],
     ];
 
     protected $expectedAbsent = ['4242', 'nonexistent'];
+
+    /**
+     * @var string
+     */
+    protected $organizationLogo;
+
+    protected function setUp(): void
+    {
+        // Make sure we have an organization logo for organization 1 by copying an existing file
+        $fileToCopy = __DIR__ . '/../../../../public/js/hv.png';
+        $organizationLogosDir = __DIR__ . '/../../../../public/images/affiliations/';
+        $this->organizationLogo = $organizationLogosDir . '1.png';
+        copy($fileToCopy, $this->organizationLogo);
+
+        // Make sure we remove the test container, since we need to rebuild it for the images to work
+        $this->removeTestContainer();
+
+        parent::setUp();
+    }
+
+    protected function tearDown(): void
+    {
+        // Remove the image again
+        unlink($this->organizationLogo);
+        $this->removeTestContainer();
+    }
 }
